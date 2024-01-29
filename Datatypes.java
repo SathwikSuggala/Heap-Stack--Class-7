498482d3affdb2e4e4534220ba05ea21a401a803
SathwikSuggala <125736441+SathwikSuggala@users.noreply.github.com>
2024-01-29 22:57:49 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,13 @@
+package coreJava;
+public class primitive{
+    public static void main(String args[]) {
+        int x = 5;
+        int y = 7;
+        System.out.println(x);
+        System.out.println(y);
+        byte b=10;
+        System.out.println(b);
+        int i=b;
+        System.out.println(i);
+        byte b2=(byte)129;
+    }}
